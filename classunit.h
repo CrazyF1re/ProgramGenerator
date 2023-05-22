1f4db2584c0c0cf177b89b260b69ca25ee04f0e3
Vova <vova.plisko@mail.ru>
2023-05-22 17:37:40 +0700

    Go back to h files only

--- classunit.h
+++ classunit.h
@@ -40,19 +40,25 @@ public:
     std::string compile(unsigned int level = 0) const
     {
         std::string result = generateShift( level ) + "class " + m_name + " {\n";
-        for( size_t i = 0; i < ACCESS_MODIFIERS.size(); ++i ) {
-        if( m_fields[ i ].empty() ) {
-        continue;
-        }
-        result += ACCESS_MODIFIERS[ i ] + ":\n";
-        for( const auto& f : m_fields[ i ] ) {
-        result += f->compile( level + 1 );
-        }
-        result += "\n";
-        }
-        result += generateShift( level ) + "};\n";
-        return result;
 
+        for( size_t i = 0; i < ACCESS_MODIFIERS.size(); ++i )
+        {
+            if( m_fields[ i ].empty() )
+            {
+                continue;
+            }
+            result += ACCESS_MODIFIERS[ i ] + ":\n";
+            for( const auto& f : m_fields[ i ] )
+            {
+                result += f->compile( level + 1 );
+            }
+
+            result += "\n";
+        }
+
+        result += generateShift( level ) + "};\n";
+
+        return result;
     }
 
 private:
--- methodunit.h
+++ methodunit.h
@@ -20,7 +20,28 @@ public:
         m_body.push_back(unit);
     }
 
-    std::string compile(unsigned int level = 0) const;
+    std::string compile(unsigned int level = 0) const
+    {
+        std::string result = generateShift(level);
+        if(m_flags & STATIC){
+            result+="static ";}
+        else if(m_flags & VIRTUAL){
+            result+="virtual ";
+        }
+        result += m_returnType+" ";
+        result += m_name + "()";
+        if(m_flags & CONST)
+        {
+            result+=" const";
+        }
+        result+=" {\n";
+        for( const auto& b:m_body)
+        {
+            result+=b->compile(level+1);
+        }
+        result+=generateShift(level)+"}\n";
+        return result;
+    }
 
 private:
     std::string m_name;
